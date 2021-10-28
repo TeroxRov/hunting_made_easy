1fabcc6b0c73a6b90b6b3217c6a29c365f4bf50f
TeroxRov <87980117+TeroxRov@users.noreply.github.com>
2021-10-27 21:22:10 -0700

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -7,5 +7,5 @@ parser.add_argument('-u', '--url',type=str,help="url to get its response")
 args = parser.parse_args()
 url = args.url
 request = requests.get(args.url)
-print(args.url,' :: ',request)
+print(args.url,' :: ',request.status_code)
 
